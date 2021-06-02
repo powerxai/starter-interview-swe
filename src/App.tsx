4242045d994b639a6857743f4661abdb390cde4d
Christopher Little-Savage <cls@powerx.ai>
2021-06-02 12:24:14 +0100

    Simplify example

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from '../public/logo512.png';
+import { Note } from './components/note';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <img src="/logo512.png" className="App-logo" alt="logo" />
+        <Note />
       </header>
     </div>
   );
---
+++ src/components/note/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Note() {
+
+
+  return (
+    <div>
+      <input type="text" value="Note goes here..." readOnly />
+    </div>
+  );
+}
